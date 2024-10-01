0e5d605d761cec6fc615d20c335db5e2726c7554
Ancr33z <nikita.borisov20011@gmail.com>
2024-10-01 15:04:21 +0300

    .

---
+++ lab14/lab14/lab14.cpp
@@ -0,0 +1,28 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <cwchar>
+#include <time.h>
+
+#include "Error.h"		//обработка ошибок
+#include "Parm.h"		//обработка параметров
+#include "Log.h"		//работа с протоколом
+#include "In.h"			//ввод исходного файла
+
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	setlocale(LC_ALL, "russian");
+
+    try {
+
+    }
+    catch (Error::ERROR e)
+    {
+
+    }
+    return 0;
+}
